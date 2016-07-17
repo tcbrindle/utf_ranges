cceb3c275bd79375e991aefe9f8fe553da3e3366
Tristan Brindle <t.c.brindle@gmail.com>
2016-07-18 03:30:37 +1200

    Add bytes_view
    This presents a view of a range as a sequence of bytes, as you
    might have guessed. The idea is that it makes it easier to pass
    to an ostream which writes out bytes to disk.
    
    I still need to work out the exact constraints on the range value
    type. SimpleType? POD? Fundemental type?

--- include/tcb/ranges/view.hpp
+++ include/tcb/ranges/view.hpp
@@ -3,6 +3,7 @@
 #define TCB_RANGES_VIEW_HPP_INCLUDED
 
 #include <tcb/ranges/view/bom.hpp>
+#include <tcb/ranges/view/bytes.hpp>
 #include <tcb/ranges/view/endian_convert.hpp>
 #include <tcb/ranges/view/line_end_transform.hpp>
 #include <tcb/ranges/view/utf_convert.hpp>
---
+++ include/tcb/ranges/view/bytes.hpp
@@ -0,0 +1,89 @@
+
+#pragma once
+
+#include <range/v3/view_adaptor.hpp>
+#include <range/v3/view/view.hpp>
+
+namespace tcb {
+namespace ranges {
+
+namespace rng = ::ranges::v3;
+using rng::static_const;
+
+template <typename Rng>
+class bytes_view : public rng::view_adaptor<bytes_view<Rng>, Rng>
+{
+private:
+    using value_type = rng::range_value_t<Rng>;
+    using byte = unsigned char;
+
+    friend rng::range_access;
+
+    struct adaptor : rng::adaptor_base
+    {
+        adaptor() = default;
+
+        adaptor(const bytes_view& b)
+        {
+            if (b.mutable_base().begin() != b.mutable_base().end()) {
+                fill_buffer(b.mutable_base().begin());
+            }
+        }
+
+        void fill_buffer(rng::range_iterator_t<Rng> it)
+        {
+            const value_type t = *it;
+            std::copy(reinterpret_cast<const byte*>(&t),
+                      reinterpret_cast<const byte*>(&t) + sizeof(value_type),
+                      buf_.begin());
+            idx_ = 0;
+        }
+
+
+        byte get(rng::range_iterator_t<Rng>) const {
+            return buf_[idx_++];
+        }
+
+        void next(rng::range_iterator_t<Rng>& it) {
+            if (idx_ == sizeof(value_type)) {
+                fill_buffer(++it);
+            }
+        }
+
+        std::array<byte, sizeof(value_type)> buf_{{}};
+        mutable int idx_ = 0;
+    };
+
+public:
+
+    bytes_view() = default;
+
+    bytes_view(Rng range)
+            : rng::view_adaptor<bytes_view, Rng>(std::move(range))
+    {}
+
+    adaptor begin_adaptor() const { return adaptor{*this}; }
+};
+
+namespace view {
+
+struct as_bytes_fn {
+
+    template <typename Rng>
+    bytes_view<rng::view::all_t<Rng>> operator()(Rng&& range) const
+    {
+        return {rng::view::all(std::forward<Rng>(range))};
+    }
+
+    decltype(auto) operator()() const
+    {
+        rng::make_pipeable(std::bind(*this));
+    }
+
+};
+
+RANGES_INLINE_VARIABLE(rng::view::view<as_bytes_fn>, as_bytes);
+
+}
+}
+}
