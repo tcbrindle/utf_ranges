6e5b0e980cd603aaeb929bb2279eb916aac2b87e
Tristan Brindle <t.c.brindle@gmail.com>
2016-07-19 01:20:21 +1200

    Example: remove unnecessary includes

--- example/utf8_to_utf16be.cpp
+++ example/utf8_to_utf16be.cpp
@@ -5,8 +5,6 @@
 #include <tcb/ranges/view.hpp>
 #include <tcb/ranges/istreambuf_range.hpp>
 #include <tcb/ranges/ostreambuf_iterator.hpp>
-#include <range/v3/istream_range.hpp>
-#include <range/v3/view/bounded.hpp>
 
 namespace rng = ::ranges::v3;
 namespace utf = ::tcb::ranges;
@@ -24,12 +22,12 @@ int main(int argc, char** argv)
     std::ifstream in_file{argv[1], std::ios::binary};
     std::ofstream out_file{argv[2], std::ios::binary};
 
-    auto view = utf::istreambuf(in_file)          // Read range from input stream
-            | utf::view::consume_bom // Remove UTF-8 "BOM" if present
-            | utf::view::as_utf16    // Convert to UTF-16
-            | utf::view::add_bom     // Prepend UTF-16 BOM to start of range
+    auto view = utf::istreambuf(in_file) // Read range from input stream
+            | utf::view::consume_bom     // Remove UTF-8 "BOM" if present
+            | utf::view::as_utf16        // Convert to UTF-16
+            | utf::view::add_bom         // Prepend UTF-16 BOM to start of range
             | utf::view::endian_convert<boost::endian::order::big> // Convert to big-endian
-            | utf::view::as_bytes;    // Write to disk as bytes
+            | utf::view::as_bytes;       // Write to disk as bytes
 
     rng::copy(view, utf::ostreambuf_iterator<char>{out_file});
 }
